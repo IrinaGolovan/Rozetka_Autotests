b7bcc84a2d0766905366b6df22ceca032109680a
irina golovan <irina.golovan.88@gmail.com>
2018-03-03 02:21:22 +0200

    TestsData added

--- src/main/java/data/PropertyValuesExtraction.java
+++
@@ -1,8 +0,0 @@
-package data;
-
-/**
- * Created by oleksandr on 17.02.2018.
- */
-public abstract class PropertyValuesExtraction {
-
-}
---
+++ src/main/java/data/TestsData.java
@@ -0,0 +1,7 @@
+package data;
+
+/**
+ * Created by oleksandr on 03.03.2018.
+ */
+public class TestsData {
+}
